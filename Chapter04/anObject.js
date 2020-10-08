3177bd687d5a2a5cc8f0d75cd419ef91d42f4f7a
Paul DE LA CELLE <pdelacelle@mantu.com>
2020-10-08 10:48:58 +0700

    ex4

---
+++ Chapter04/anObject.js
@@ -0,0 +1,10 @@
+let anObject = {left: 1, right: 2};
+console.log(anObject.left);
+// → 1
+delete anObject.left;
+console.log(anObject.left);
+// → undefined
+console.log("left" in anObject);
+// → false
+console.log("right" in anObject);
+// → true
