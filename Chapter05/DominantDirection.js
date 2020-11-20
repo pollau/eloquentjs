981ae718dc1f4cee960bd7c53e97b248732f2f94
Paul DE LA CELLE <paul.delacelle@gmail.com>
2020-11-20 16:43:10 +0700

    adding script

---
+++ Chapter05/DominantDirection.js
@@ -0,0 +1,31 @@
+
+var SCRIPTS = require("./scripts");
+
+function characterScript(code) {
+    for (let script of SCRIPTS) {
+      if (script.ranges.some(([from, to]) => {
+        return code >= from && code < to;
+      })) {
+        return script;
+      }
+    }
+    return null;
+  }
+  
+  function countBy(items, groupName) {
+    let counts = [];
+    for (let item of items) {
+      let name = groupName(item);
+      let known = counts.findIndex(c => c.name == name);
+      if (known == -1) {
+        counts.push({name, count: 1});
+      } else {
+        counts[known].count++;
+      }
+    }
+    return counts;
+  }
+
+
+  console.log(characterScript(121));
+  // → {name: "Latin", …}
